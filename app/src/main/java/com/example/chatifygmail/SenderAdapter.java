7782eaf136f64c024fbbd84b458b561d2c543ccf
Raghu Chandan <33779280+Raghu-Chandan@users.noreply.github.com>
2020-07-08 19:50:42 +0530

    Added the Adapter for Sender

---
+++ app/src/main/java/com/example/chatifygmail/SenderAdapter.java
@@ -0,0 +1,92 @@
+package com.example.chatifygmail;
+
+import android.content.Context;
+import android.graphics.drawable.GradientDrawable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.chatifygmail.database.Sender;
+
+import java.util.List;
+
+public class SenderAdapter extends RecyclerView.Adapter<SenderAdapter.SenderViewHolder> {
+
+    final private ItemClickListener mItemClickListener;
+    private List<Sender> senders;
+    private Context mContext;
+
+    public SenderAdapter(Context context, ItemClickListener listener){
+        mContext = context;
+        mItemClickListener = listener;
+    }
+
+    @NonNull
+    @Override
+    public SenderViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(mContext)
+                .inflate(R.layout.sender_layout, parent, false);
+
+        return new SenderViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull SenderViewHolder holder, int position) {
+        Sender sender = senders.get(position);
+        String emailAddress = sender.getEmailAddress();
+        int unread = sender.getUnread();
+
+        holder.emailTextView.setText(emailAddress);
+        String unreadString = "" + unread; // converts int to String
+        holder.unreadView.setText(unreadString);
+
+        GradientDrawable priorityCircle = (GradientDrawable) holder.unreadView.getBackground();
+        // Get the appropriate background color based on the priority
+        int unreadColor = R.color.colorPrimaryDark;
+        priorityCircle.setColor(mContext.getResources().getColor(unreadColor) );
+    }
+
+    @Override
+    public int getItemCount() {
+        if (senders == null) {
+            return 0;
+        }
+        return senders.size();
+    }
+
+    public List<Sender> getSenders() {
+        return senders;
+    }
+
+    public void setSenders(List<Sender> senders) {
+        this.senders = senders;
+    }
+
+    class SenderViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+        TextView emailTextView;
+        TextView unreadView;
+
+        public SenderViewHolder(@NonNull View itemView) {
+            super(itemView);
+            emailTextView = itemView.findViewById(R.id.email_text_view);
+
+            unreadView = itemView.findViewById(R.id.unread_text_view);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            String emailAddress = senders.get(getAdapterPosition()).getEmailAddress();
+            mItemClickListener.onItemClickListener(emailAddress);
+        }
+    }
+
+    public interface ItemClickListener {
+        void onItemClickListener(String emailAddress);
+    }
+}
